ebe685de8c035631e88008b89023c73c6dc75b1a
Raúl Sánchez Ruiz <ulthuan@gmail.com>
2019-03-27 22:49:49 +0100

    add models webhooks

--- models/webhook.go
+++ models/webhook.go
@@ -2,12 +2,12 @@ package models
 
 import (
 	"encoding/json"
+	"image/color"
 	"time"
 
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/validate"
 	"github.com/gofrs/uuid"
-	"github.com/ulthuan/go-glo"
 )
 
 type WebhookBoard struct {
@@ -27,22 +27,75 @@ type WebhookArchived struct {
 	Sender WebhookSender `json:"sender"`
 }
 
-type WebhookCardAdded struct{}
-type WebhookCardUpdate struct{}
-type WebhookCardCopied struct{}
+type WebhookLabel struct {
+	ID    string     `json:"id"`
+	Name  string     `json:"name"`
+	Color color.RGBA `json:"color"`
+}
+
+type WebhookPartialUser struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+}
+
+type WebhookDescription struct {
+	Text string `json:"text"`
+}
+
+type WebhookPartialCard struct {
+	ID          string             `json:"id"`
+	Name        string             `json:"name"`
+	Description WebhookDescription `json:"description"`
+}
+
+type WebhookLabels struct {
+	Added   []WebhookLabel `json:"added"`
+	Removed []WebhookLabel `json:"removed"`
+}
+
+type WebhookAssignees struct {
+	Added   []WebhookPartialUser `json:"added"`
+	Removed []WebhookPartialUser `json:"removed"`
+}
+
+type WebhookCard struct {
+	ID                 string             `json:"id"`
+	Name               string             `json:"name"`
+	CreatedDate        time.Time          `json:"created_date"`
+	BoardID            string             `json:"board_id"`
+	ColumnID           string             `json:"column_id"`
+	Labels             []WebhookLabel     `json:"labels"`
+	Assignees          []WebhookAssignees `json:"assignees"`
+	CompletedTaskCount int                `json:"completed_task_count"`
+	TotalTaskCount     int                `json:"total_task_count"`
+	AttachmentCount    int                `json:"attachment_count"`
+	CommentCount       int                `json:"comment_count"`
+	Description        WebhookDescription `json:"description"`
+	CreatedBy          WebhookPartialUser `json:"created_by"`
+	Previous           WebhookPartialCard `json:"previous"`
+	Position           int                `json:"position"`
+}
+
+type WebhookCardEvent struct {
+	Action    string           `json:"action"`
+	Board     WebhookBoard     `json:"board"`
+	Sender    WebhookSender    `json:"sender"`
+	Card      WebhookCard      `json:"card"`
+	NewCard   WebhookCard      `json:"new_card"`
+	OldCard   WebhookCard      `json:"old_card"`
+	Labels    WebhookLabels    `json:"labels"`
+	Assigness WebhookAssignees `json:"assignees"`
+	Sequence  int              `json:"sequence"`
+}
 
 type Webhook struct {
-	ID             uuid.UUID       `json:"id" db:"id"`
-	Action         string          `json:"action" db:"action"`
-	BoardID        string          `json:"board.id" db:"board_id"`
-	Board          glo.Board       `json:"board"`
-	Sender         glo.User        `json:"sender"`
-	SenderUsername string          `json:"sender.username" db:"sender_username"`
-	Members        []glo.Member    `json:"members"`
-	Column         glo.BoardColumn `json:"column"`
-
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID             uuid.UUID `json:"id" db:"id"`
+	Action         string    `json:"action" db:"action"`
+	BoardID        string    `json:"board_id" db:"board_id"`
+	CardID         string    `json:"card_id" db:"card_id"`
+	SenderUsername string    `json:"sender_username" db:"sender_username"`
+	CreatedAt      time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
 }
 
 // String is not required by pop and may be deleted
