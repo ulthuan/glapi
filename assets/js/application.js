6d716b0fc5833131bdbb19f93b284358394c7e48
Raúl Sánchez Ruiz <ulthuan@gmail.com>
2019-04-06 20:15:49 +0200

    add event checkbox

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,5 +2,9 @@ require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 require("./sb-admin-2.min.js");
 $(() => {
-
+    jQuery(document).ready(function(){
+        $("div.p-switch input[type=checkbox]").change(function(){
+            window.location.replace("/auth/scm/"+$(this).attr("name"));
+        });
+    })
 });
